5e726304975529cfc9a50ec0c04c2654cccd236a
Matías Gaete <matiasgf18@gmail.com>
2019-07-26 17:12:33 -0400

    wip: form2

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   resources :users
+  delete 'products/delete', action: :delete, controller: 'products'
   get 'home/index'
   get 'home/timestamp'
-  get 'home/about'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
