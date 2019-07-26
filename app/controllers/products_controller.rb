401d6bd6ce05af0ef2114cb896855b472b96abc1
Matías Gaete <matiasgf18@gmail.com>
2019-07-26 17:28:50 -0400

    feature: some styles yikes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
+    @product.user_id = 1
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :user_id, :category_id, :stock, :expiration_date, :image)
+      params.require(:product).permit(:title, :description, :category_id, :stock, :expiration_date, :image)
     end
 end
