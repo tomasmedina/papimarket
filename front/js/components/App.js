fc5a298aeae7c7d9a5f27a2588000859d4acb6cd
tmedina <tomasmedina.a@gmail.com>
2019-07-26 14:55:52 -0400

    init commit

--- front/js/components/App.js
+++ front/js/components/App.js
@@ -3,14 +3,15 @@
  */
 
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 import Timestamp from './Timestamp';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
-
+      <h1>Test</h1>
+      <Button>hola</Button>
       <Timestamp />
     </div>
   );
